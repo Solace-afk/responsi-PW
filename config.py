83b95ebf61afd9efe91a008f6f5cb1c91272051f
Solace-afk <rojalinur50@gmail.com>
2026-07-23 23:16:46 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config:
+    """
+    Konfigurasi aplikasi Flask
+    """
+
+    # Konfigurasi Database MySQL
+    HOST = "solace-alamsyahn51-e2f4.k.aivencloud.com"
+    PORT = 20678
+    USER = "avnadmin"
+    PASSWORD = "AVNS_Cip9U3cWCD072aBTiKy"
+    DATABASE = "db_akademik"
+
+    # Secret Key Flask
+    SECRET_KEY = "praktikum-flask-2026"
+
+    # Konfigurasi Flask
+    DEBUG = True
